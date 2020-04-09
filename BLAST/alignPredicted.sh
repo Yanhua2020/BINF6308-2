7a843aed3b5bb7004e3b27efa943eb7a9177d80b
Yanhua2020 <gao.yanh@husky.neu.edu>
2020-04-09 16:44:17 -0400

    name change

--- BLAST/alignPredicted.sh
+++ BLAST/alignPredicted.sh
@@ -3,4 +3,4 @@
 blastp -query Trinity.fasta.transdecoder.pep \
 -db swissprot -evalue 1e-10 \
 -outfmt "6 qseqid sacc qlen slen length nident pident evalue stitle" \
-1>alignPreicted.txt 2>alignPredicted.err 
+1>alignPredicted.txt 2>alignPredicted.err 
