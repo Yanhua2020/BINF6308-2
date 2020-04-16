fb7893f21e8b393e20a98949dfa777275116859b
Yanhua2020 <gao.yanh@husky.neu.edu>
2020-04-15 23:46:01 -0400

    ProteinClassification

--- ProteinClassification/removeStop.sh
+++ ProteinClassification/removeStop.sh
@@ -2,4 +2,4 @@
 #removeStop.sh
 
 sed 's/*//' ../BLAST/Trinity.fasta.transdecoder.pep > output.fasta
-head output.fasta -n 500500500500500 > proteins.fasta
+head output.fasta -n 500 > proteins.fasta
--- ProteinClassification/runIPS.sh
+++ ProteinClassification/runIPS.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 #runIPS.sh
 
-
-
-my_interproscan5.17-56.0/interproscan-5.17-56.0/interproscan.sh \
+/usr/local/programs/interproscan-5.33-72.0/interproscan.sh \
 -i proteins.fasta \
--b proteins.tsv
+-o proteins.tsv \
+-f TSV
   
 
 
